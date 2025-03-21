03f39c9569a84cec2f4565d91efd59bfb9e29d7f
LuckedCoronet45 <lc45@lc-studios.net>
2025-03-21 14:27:41 +0900

    feat(lib): expose some api

---
+++ src/lib/index.ts
@@ -0,0 +1,3 @@
+export { mod2jsonSingle, mod2jsonMultiple } from "@shared/mod2json/public";
+export { Config } from "@shared/config";
+export { compilePack, CompilePackOpts } from "@shared/compiler";
