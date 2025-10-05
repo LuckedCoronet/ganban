478b1dd95fc3f5f83f2783448dece3cc4a5e2ba1
LuckedCoronet <lc45@lc-studios.net>
2025-10-06 08:42:45 +0900

    Update build.ts

--- src/build.ts
+++ src/build.ts
@@ -1,15 +1,17 @@
 import type { BuildConfig, PackConfig } from "./config";
+import { createLogger, type Logger } from "./utils/logger";
 
 type PackBuildContext = {
 	buildConfig: BuildConfig;
 	packConfig: PackConfig;
+	log: Logger;
 	signal?: AbortSignal;
 };
 
 type PackBuildResult = void;
 
 const buildPack = async (ctx: PackBuildContext): Promise<PackBuildResult> => {
-	const { buildConfig, packConfig, signal } = ctx;
+	const { buildConfig, packConfig, log, signal } = ctx;
 
 	signal?.throwIfAborted();
 };
@@ -17,31 +19,52 @@ const buildPack = async (ctx: PackBuildContext): Promise<PackBuildResult> => {
 export const build = async (config: BuildConfig, signal?: AbortSignal): Promise<void> => {
 	signal?.throwIfAborted();
 
+	const minLogLevel = config.logLevel;
+
+	const globalLogger = createLogger({
+		minLevel: minLogLevel,
+	});
+
 	if (!config.behaviorPack && !config.resourcePack) {
-		throw new Error("Neither behaviorPack nor resourcePack is configured.");
+		globalLogger.warn("Neither behaviorPack nor resourcePack is configured.");
+		return;
 	}
 
-	const packBuildResults: Promise<PackBuildResult>[] = [];
+	const packBuildPromises: Promise<PackBuildResult>[] = [];
 
 	if (config.behaviorPack) {
-		packBuildResults.push(
+		packBuildPromises.push(
 			buildPack({
 				buildConfig: config,
 				packConfig: config.behaviorPack,
+				log: createLogger({
+					minLevel: minLogLevel,
+					prefix: "behaviorPack",
+				}),
 				signal,
 			}),
 		);
+
+		globalLogger.debug("Added behavior pack build promise into the array");
 	}
 
 	if (config.resourcePack) {
-		packBuildResults.push(
+		packBuildPromises.push(
 			buildPack({
 				buildConfig: config,
 				packConfig: config.resourcePack,
+				log: createLogger({
+					minLevel: minLogLevel,
+					prefix: "resourcePack",
+				}),
 				signal,
 			}),
 		);
+
+		globalLogger.debug("Added resource pack build promise into the array");
 	}
 
-	const settledResults = await Promise.allSettled(packBuildResults);
+	const settledResults = await Promise.allSettled(packBuildPromises);
+
+	globalLogger.debug(`All ${packBuildPromises.length} pack build promise(s) has settled`);
 };
