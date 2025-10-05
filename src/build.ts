05846d5aa642bd1fff0f7d941705992ea12dc9e4
LuckedCoronet <lc45@lc-studios.net>
2025-10-06 07:46:38 +0900

    Update build.ts

--- src/build.ts
+++ src/build.ts
@@ -1,4 +1,47 @@
-import type { BuildConfig } from "./config";
+import type { BuildConfig, PackConfig } from "./config";
+
+type PackBuildContext = {
+	buildConfig: BuildConfig;
+	packConfig: PackConfig;
+	signal?: AbortSignal;
+};
+
+type PackBuildResult = void;
+
+const buildPack = async (ctx: PackBuildContext): Promise<PackBuildResult> => {
+	const { buildConfig, packConfig, signal } = ctx;
+
+	signal?.throwIfAborted();
+};
 
 export const build = async (config: BuildConfig, signal?: AbortSignal): Promise<void> => {
+	signal?.throwIfAborted();
+
+	if (!config.behaviorPack && !config.resourcePack) {
+		throw new Error("Neither behaviorPack nor resourcePack is configured.");
+	}
+
+	const packBuildResults: Promise<PackBuildResult>[] = [];
+
+	if (config.behaviorPack) {
+		packBuildResults.push(
+			buildPack({
+				buildConfig: config,
+				packConfig: config.behaviorPack,
+				signal,
+			}),
+		);
+	}
+
+	if (config.resourcePack) {
+		packBuildResults.push(
+			buildPack({
+				buildConfig: config,
+				packConfig: config.resourcePack,
+				signal,
+			}),
+		);
+	}
+
+	const settledResults = await Promise.allSettled(packBuildResults);
 };
